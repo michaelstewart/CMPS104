027bdb39568f39d292db796dbc42df4a3cb15cec
Michael Stewart <mijstewa@ucsc.edu>
2013-10-11 18:22:55 -0700

    fixed flags, moved ccp call parsing to oc

--- oc.cc
+++ oc.cc
@@ -1,5 +1,3 @@
-// $Id: main.cc,v 1.3 2013-09-23 14:39:10-07 - - $
-
 #include <string>
 using namespace std;
 
@@ -12,63 +10,126 @@ using namespace std;
 
 #include "auxlib.h"
 #include "stringset.h"
-#include "cppstrtok.h"
 
-const string CPP = "/usr/bin/cpp";
+string CPP = "/usr/bin/cpp";
+const size_t LINESIZE = 1024;
 const string OPT_STRING = "-lyD:@:";
 int option;
+int file_arg_i = 0;
+int opterr = 0;
+
+// Run cpp against the lines of the file.
+void cpplines (FILE *pipe) {
+   for (;;) {
+      char buffer[LINESIZE];
+      char *fgets_rc = fgets (buffer, LINESIZE, pipe);
+      if (fgets_rc == NULL) break;
+
+      if (*buffer == '#') {
+         // Skip line
+         continue;
+      }
+
+      char *savepos = NULL;
+      char *bufptr = buffer;
+      for (;;) {
+         char *token = strtok_r (bufptr, " \t\n", &savepos);
+         bufptr = NULL;
+         if (token == NULL) break;
+         intern_stringset (token);
+      }
+   }
+}
 
 int main (int argc, char** argv) {
    set_execname (argv[0]);
 
    while ( (option = getopt(argc, argv, OPT_STRING.c_str())) != -1 )  {
       switch(option) {
+         case '?':
+            fprintf(stderr, 
+               "Usage: oc [-ly] [-@ flag] [-D string] program.oc\n");
+            set_exitstatus(2);
+            return get_exitstatus();
+            break;
          case 'l':
-            printf("%s\n", "l");
+            // enable this when flex is being used.
+            // yy_flex_debug = 1;
             break;
          case 'y':
-            printf("%s\n", "y");
+            // enable this when flex is being used.
+            // yydebug = 1;
             break;
          case 'D':
-            printf("%s\n", optarg);
+            CPP += " -D" + string(optarg);
             break;
          case '@':
-            printf("%s\n", optarg);
+            set_debugflags(optarg);
+            break;
+         case 1:
+            // Set the index of the program file
+            if (file_arg_i) {
+               fprintf(stderr, 
+                  "Usage: oc [-ly] [-@ flag] [-D string] program.oc\n");
+               set_exitstatus(2);
+               return get_exitstatus();
+            }
+            file_arg_i = optind-1;
             break;
       }
    }
 
-   char* filename = argv[argc-1]; 
-   char* ext;
-   // char* p;
-   
-   ext = strchr(filename, '.');
-   char* program_name = strndup(filename, ext-filename);
-   // printf("extension - %s\nfilename - ",ext+1);
-   if (!strcmp(ext+1, "oc")) {         
-      printf("%s \n", program_name);
-   } else {
-      fprintf(stderr, "oc accepts .oc files only \n");
-      return 1;
-   }    
+   if (!file_arg_i) {
+      fprintf(stderr, 
+         "Usage: oc [-ly] [-@ flag] [-D string] program.oc\n");
+      set_exitstatus(2);
+      return get_exitstatus();
+   }
+   char* filename = argv[file_arg_i];
+   char* filebase = basename(filename);
+   char* ext = strchr(filebase, '.');
+   if (ext == NULL) {
+      fprintf(stderr, "Error: oc accepts .oc files only \n");
+      set_exitstatus(3);
+      return get_exitstatus();
+   }
+   char* program_name = strndup(filebase, ext-filebase);
 
-   string str_path = string(program_name);
-   str_path = str_path + ".str";
+   // Remove all trailing slashes
+   for (int i = strlen(ext+1); i > 0; i--) {
+      if (ext[i] == '/') {
+         ext[i] = '\0';
+      } else {
+         break;
+      }
+   }
+
+   // Check file extension
+   if (strcmp(ext+1, "oc")) {
+      fprintf(stderr, "Error: oc accepts .oc files only \n");
+      set_exitstatus(3);
+      return get_exitstatus();
+   }
+
+   // Open up the output file.
+   string str_path = string(program_name) + ".str";
    FILE* str_file = fopen(str_path.c_str(), "w");
 
+   // Open a pipe to cpp
    string command = CPP + " " + filename;
    FILE* pipe = popen (command.c_str(), "r");
 
    if (pipe == NULL) {
       syserrprintf (command.c_str());
-   }else {
-      cpplines (pipe, filename);
-      int pclose_rc = pclose (pipe);
-      eprint_status (command.c_str(), pclose_rc);
+      set_exitstatus(4);
+   } else {
+      cpplines (pipe);
+      if (pclose (pipe) != 0) {
+         set_exitstatus(1);
+      }
    }
-      
+
    dump_stringset (str_file);
    fclose(str_file);
-
    return get_exitstatus();
 }
